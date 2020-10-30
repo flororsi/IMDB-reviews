438f0f1e382a7d21e656194801992db00a0f3153
flororsi <flororsi@gmail.com>
2020-10-30 18:49:42 -0300

    final changes

--- src/pages/Results/index.js
+++ src/pages/Results/index.js
@@ -1,23 +1,43 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { Container } from '@material-ui/core'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Container, CircularProgress } from '@material-ui/core'
 import queryString from 'query-string'
 
 import { searchMovie } from '../../redux/actions/search'
+import { movieResults, isSearchLoading } from '../../redux/selectors'
+import MovieResult from '../../components/MovieResult'
 
-function index({location}) {
+export default ({location}) => {
     const dispatch = useDispatch()
+    const movies = useSelector(state=> movieResults(state))
+    const isLoading = useSelector(state => isSearchLoading(state))
+    const [isLooked, setIsLooked] = useState(false)
     
     useEffect(() => {
         const { movieName } = queryString.parse(location.search)
-        dispatch(searchMovie({movieName}))
+
+        if (movieName && !isLooked){
+            setIsLooked(true)
+            dispatch(searchMovie({movieName}))
+        }
+
     })
+
+    const renderMovies = () => {
+        if (movies){
+            return movies.map((value, index) => 
+            <MovieResult key ={index} {...value}/>);
+        }else if (isLoading){
+            return <CircularProgress  size={100} color="primary"/>
+        }
+        return <div/>
+    }
 
     return (
         <Container>
-            Results
+            {renderMovies()}
         </Container>
     )
 }
 
-export default index
+// export default index
